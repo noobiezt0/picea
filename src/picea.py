310521a6f6a3d4f43896fbf80f5e2f137726a58b
noobiezt0 <bair2010g@gmail.com>
2025-05-17 18:41:23 +0800

    Move class DropdownMenu to picea.py

--- src/picea.py
+++ src/picea.py
@@ -1,7 +1,5 @@
-from customtkinter import CTkFrame, CTkButton, CTkTextbox, CTkScrollbar
-from dropdown_menu import CTkDropdownMenu
-
-# Textbox
+from customtkinter import CTkFrame, CTkButton, CTkTextbox, CTkScrollbar, CTkLabel
+from customtkinter.windows.widgets.core_widget_classes import DropdownMenu
 
 class Textbox(CTkFrame):
     def __init__(self, *args, **kwargs):
@@ -14,6 +12,19 @@ class Textbox(CTkFrame):
         self.scrollbar.pack(side='right', fill='y', padx=3, pady=(0, 3))
         self.textbox.pack(side='right', fill='both', pady=(0, 3), expand=True)
         self.linenums.pack(side='left', fill='y', padx=3, pady=(0, 3))
+
+
+class CTkDropdownMenu(DropdownMenu):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._add_menu_commands()
+        self._add_separators()
+    def _add_separators(self):
+        for value in self._values:
+            if value == '***':
+                index = self._values.index('***')
+                self.delete(index, index)
+                self.insert(index, itemType='separator')
 
 class MenuCascade(CTkButton):
     def __init__(self, master=None, values: list | None = None, *args, **kwargs):
@@ -37,5 +48,13 @@ class Menubar(CTkFrame):
         self.window = None
 
     def add_cascade(self, name: str, values: list):
-        self.menu[name] = MenuCascade(self, width=0, text=name, values=values, corner_radius=0)
+        self.menu[name] = MenuCascade(
+            self,
+            width=0,
+            text=name,
+            values=values,
+            corner_radius=0,
+            fg_color=('white', '#000000'),
+            hover_color=('f0f0f0', '#121212')
+        )
         self.menu[name].pack(side='left', anchor='nw', ipadx=8)

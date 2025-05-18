05943d154b01a96ca833c8af9feb0984ab755eec
noobiezt0 <bair2010g@gmail.com>
2025-05-18 15:29:54 +0800

    Add new functions

--- src/picea.py
+++ src/picea.py
@@ -9,10 +9,13 @@ class WelcomePage(CTkFrame):
     def add(self, widget):
         self._widgets.append(widget)
 
-    def pack_all(self, **kwargs):
+    def cfg_buttons(self, **kwargs):
         for widget in self._widgets:
             if isinstance(widget, CTkButton):
-                widget.configure(anchor='w')
+                widget.configure(**kwargs)
+
+    def pack_all(self, **kwargs):
+        for widget in self._widgets:
             widget.pack(**kwargs)
 
 class Textbox(CTkFrame):
@@ -22,9 +25,14 @@ class Textbox(CTkFrame):
         self.scrollbar = CTkScrollbar(self, command=self.textbox.yview, width=6)
         self.textbox.configure(yscrollcommand=self.scrollbar.set)
         self.linenums = CTkTextbox(self, activate_scrollbars=False, width=64, text_color='#969696')
-        self.statusbar = CTkFrame(self, height=16)
 
+        self.statusbar = Menubar(self, height=16, fg_color="transparent")
         self.statusbar.pack(side='bottom', fill='x', padx=3, pady=(0, 3))
+        self.statusbar.add_cascade('Encoding')
+        self.statusbar.add_cascade('CRLF')
+        self.statusbar.add_cascade('1:0')
+        self.statusbar.pack_cascades(side='right', ipadx=8)
+
         self.scrollbar.pack(side='right', fill='y', padx=3, pady=(0, 3))
         self.textbox.pack(side='right', fill='both', pady=(0, 3), expand=True)
         self.linenums.pack(side='left', fill='y', padx=3, pady=(0, 3))
@@ -58,16 +66,20 @@ class Menubar(CTkFrame):
         self.menu = dict()
         self.window = None
 
-    def add_cascade(self, name: str):
+    def add_cascade(self, name: str, **kwargs):
         self.menu[name] = MenuCascade(
             self,
             width=0,
             text=name,
             corner_radius=0,
-            fg_color=('white', '#000000'),
-            hover_color=('f0f0f0', '#121212')
+            fg_color="transparent",
+            hover_color=('#f0f0f0', '#121212'),
+            **kwargs
         )
-        self.menu[name].pack(side='left', anchor='nw', ipadx=8)
+
+    def pack_cascades(self, *args, **kwargs):
+        for cascade in self.menu:
+            self.menu[cascade].pack(*args, **kwargs)
 
     def add_command(self, cascade: str, **kwargs):
         self.menu[cascade].add_command(**kwargs)

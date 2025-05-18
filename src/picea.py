74ac255dc27141f0be7a161da21900e67f15a954
noobiezt0 <bair2010g@gmail.com>
2025-05-18 14:29:47 +0800

    Menubar code rewritten, add statusbar to Textbox

--- src/picea.py
+++ src/picea.py
@@ -22,7 +22,9 @@ class Textbox(CTkFrame):
         self.scrollbar = CTkScrollbar(self, command=self.textbox.yview, width=6)
         self.textbox.configure(yscrollcommand=self.scrollbar.set)
         self.linenums = CTkTextbox(self, activate_scrollbars=False, width=64, text_color='#969696')
+        self.statusbar = CTkFrame(self, height=16)
 
+        self.statusbar.pack(side='bottom', fill='x', padx=3, pady=(0, 3))
         self.scrollbar.pack(side='right', fill='y', padx=3, pady=(0, 3))
         self.textbox.pack(side='right', fill='both', pady=(0, 3), expand=True)
         self.linenums.pack(side='left', fill='y', padx=3, pady=(0, 3))
@@ -31,22 +33,11 @@ class Textbox(CTkFrame):
 class CTkDropdownMenu(DropdownMenu):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._add_menu_commands()
-        self._add_separators()
-    def _add_separators(self):
-        for value in self._values:
-            if value == '***':
-                index = self._values.index('***')
-                self.delete(index, index)
-                self.insert(index, itemType='separator')
 
 class MenuCascade(CTkButton):
-    def __init__(self, master=None, values: list | None = None, *args, **kwargs):
-        super().__init__(master=master, *args, **kwargs)
-        self._values = values
-        self._dropdown_menu = CTkDropdownMenu(
-            master=self, values=self._values
-        )
+    def __init__(self, master=None, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self._dropdown_menu = CTkDropdownMenu(self, values=list())
         self.configure(command=self._open_dropdown_menu)
 
     def _open_dropdown_menu(self):
@@ -55,20 +46,31 @@ class MenuCascade(CTkButton):
             self.winfo_rooty() + self._apply_widget_scaling(self._current_height)
         )
 
+    def add_command(self, *args, **kwargs):
+        self._dropdown_menu.add_command(*args, **kwargs)
+
+    def add_separator(self, *args, **kwargs):
+        self._dropdown_menu.add_separator(*args, **kwargs)
+
 class Menubar(CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.menu = dict()
         self.window = None
 
-    def add_cascade(self, name: str, values: list):
+    def add_cascade(self, name: str):
         self.menu[name] = MenuCascade(
             self,
             width=0,
             text=name,
-            values=values,
             corner_radius=0,
             fg_color=('white', '#000000'),
             hover_color=('f0f0f0', '#121212')
         )
         self.menu[name].pack(side='left', anchor='nw', ipadx=8)
+
+    def add_command(self, cascade: str, **kwargs):
+        self.menu[cascade].add_command(**kwargs)
+
+    def add_separator(self, cascade: str, **kwargs):
+        self.menu[cascade].add_separator(**kwargs)

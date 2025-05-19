3320c370130cb7be63fc24e444ccc39079b485f8
noobiezt0 <bair2010g@gmail.com>
2025-05-19 19:19:38 +0800

    Reorder and abies integration

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
+from customtkinter import *
+
+from pinaceae.abies.abies import abies
+from pinaceae.textbox import TextboxFrame
+from pinaceae.menu import Menubar
+
 from tkinter import PhotoImage
 
-from customtkinter import *
-from picea import *
 import sys
+import os
 
-set_default_color_theme('../res/theme/default.json')
+set_default_color_theme('../res/themes/default.json')
 set_appearance_mode('system')
 
 class App(CTk):
@@ -12,65 +17,76 @@ class App(CTk):
         super().__init__()
         self.title('Picea Notepad')
         if sys.platform.startswith('win'):
-            self.iconbitmap('../res/icon.ico')
+            self.iconbitmap('../res/icon/icon.ico')
         else:
-            self.iconphoto(False, PhotoImage(file='../res/icon.png'))
+            self.iconphoto(False, PhotoImage(file='../res/icon/icon.png'))
         self.geometry('800x600')
 
-        self.menubar = Menubar(self, fg_color=('white', '#000000'))
-        self.menubar.pack(side='top', anchor='nw', fill='x')
+        self.layouts = dict()
 
+        self.tabview = CTkTabview(self, anchor='nw', fg_color=('#e0e0e0', 'black'))
+        self.tabs = dict()
+
+        self.menubar = Menubar(self, fg_color=('white', 'black'))
         self.menubar.add_cascade('File')
         self.menubar.add_cascade('Edit')
         self.menubar.add_cascade('Help')
-        self.menubar.pack_cascades(side='left', ipadx=8)
 
-        self.menubar.add_command('File', label='New', command=self.new_tab)
+        self.menubar.add_command('File', label='New', command=self.new_tab, accelerator='Ctrl+N')
+        self.menubar.add_command('File', label='Open', accelerator='Ctrl+O')
         self.menubar.add_separator('File')
         self.menubar.add_command('File', label='Exit', command=exit)
 
-        self.tabview = CTkTabview(self, anchor='nw', fg_color=('#e0e0e0', '#060606'))
+        self.menubar.add_command('Help', label='About')
+
+        layouts = os.listdir('../res/layouts/')
+
+        for layout in layouts:
+            with open(f'../res/layouts/{layout}') as layoutf:
+                self.layouts[layout.removesuffix('.json')] = layoutf.read()
+
+        self.menubar.pack(side='top', anchor='nw', fill='x')
+        self.menubar.pack_cascades(side='left', ipadx=8)
+
         self.tabview.pack(side='bottom', fill='both', expand=True)
+        self.new_tab(None, name='Welcome')
 
-        self.tabl = dict()
-        self.welcome_page()
-
-        self.bind('<Control-N>', self.new_tab)
         self.bind('<Control-n>', self.new_tab)
-
-    def welcome_page(self):
-        self.tabview.add('Welcome')
-        welcome_page = WelcomePage(self.tabview.tab('Welcome'))
-        welcome_page.add(CTkLabel(welcome_page, text='\nPicea Notepad\n', font=CTkFont(size=36)))
-        welcome_page.add(CTkLabel(welcome_page, text='Start', font=CTkFont(size=24)))
-        welcome_page.add(CTkButton(welcome_page, text='New', anchor='w', command=self.new_tab))
-        welcome_page.add(CTkButton(welcome_page, text='Open', anchor='w'))
-        welcome_page.add(CTkLabel(welcome_page, text='Recent', font=CTkFont(size=24)))
-        welcome_page.add(CTkButton(welcome_page, anchor='w'))
-        welcome_page.add(CTkButton(welcome_page, anchor='w'))
-        welcome_page.add(CTkButton(welcome_page, anchor='w'))
-        welcome_page.cfg_buttons(anchor='w', fg_color='transparent', hover_color=("#e0e0e0", "#242424"))
-        welcome_page.pack_all(anchor='nw', pady=3, padx=24)
-        welcome_page.pack(fill='both', expand=True)
 
     def new_tab(self, event=None, **kwargs):
         if 'name' in kwargs:
             name = kwargs['name']
         else:
             name = 'New'
+
+        if name not in self.tabs:
+            self.tabs[name] = dict()
+
         try:
             self.tabview.add(name)
-            self.tabl[name] = dict()
-            self.tabl[name]['count'] = 0
+            if 'count' not in self.tabs[name]:
+                self.tabs[name]['count'] = 0
         except ValueError:
-            self.tabl[name]['count'] += 1
-            name = f'{name} ({self.tabl[name]['count']})'
+            self.tabs[name]['count'] += 1
+            name = f'{name} ({self.tabs[name]['count']})'
+            self.tabs[name] = dict()
             self.tabview.add(name)
-        self.tabl[name]['content'] = Textbox(self.tabview.tab(name), fg_color=('#e0e0e0', '#060606'))
-        if sys.platform.startswith('win'):
-            self.tabl[name]['content'].textbox.configure(font=CTkFont(family='Cascadia Mono', size=14))
-            self.tabl[name]['content'].linenums.configure(font=CTkFont(family='Cascadia Mono', size=14))
-        self.tabl[name]['content'].pack(fill='both', expand=True)
+
+        if name == 'Welcome':
+            self.tabs[name]['frame'] = CTkFrame(self.tabview.tab(name))
+            layout = abies(self.tabs[name]['frame'])
+            layout.load(self.layouts['welcome'])
+            layout.widgets['Button-New'].configure(command=self.new_tab)
+            CTkButton(self.tabs[name]['frame'], anchor='nw')
+            CTkButton(self.tabs[name]['frame'], anchor='nw')
+            CTkButton(self.tabs[name]['frame'], anchor='nw')
+            self.tabs[name]['frame'].pack(fill='both', expand=True)
+        else:
+            self.tabs[name]['frame'] = TextboxFrame(self.tabview.tab(name), fg_color=('#e0e0e0', 'black'))
+            self.tabs[name]['frame'].textbox.configure(font=CTkFont('Consolas', size=16))
+            self.tabs[name]['frame'].linenums.configure(font=CTkFont('Consolas', size=16))
+
+        self.tabs[name]['frame'].pack(fill='both', expand=True)
         self.tabview.set(name)
 
 if __name__ == '__main__':
